388435719a3088884701646e3715f8d6af0f0859
Programacion <programacion@brandgroup.com.py>
2022-10-12 07:48:03 -0300

    commit de la tarea

---
+++ bucle/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+       for (int con = 1; con < 10 ;con=con+1) {
+           System.out.println(con);
+       }
+       int contador=1;
+       while (contador!=15){
+           System.out.println("abrigo");
+           contador=contador+1;
+
+       }
+    }
+}
---
+++ ejercicios_tema_4/src/condicional_if.java
@@ -0,0 +1,11 @@
+public class condicional_if {
+    public static void main(String[] args) {
+        int numero=-1;
+        if (numero>0) {
+            System.out.println(numero+" El nro es positivo");
+        }else{
+
+            System.out.println(numero+" El nro es negativo");
+        }
+    }
+}
