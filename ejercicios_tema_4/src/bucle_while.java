99c79939a43ac7edb9b6c16f63ceaf4a8915fb59
Programacion <programacion@brandgroup.com.py>
2022-10-12 07:46:22 -0300

    commit de la tarea

---
+++ ejercicios_tema_4/src/bucle_while.java
@@ -0,0 +1,8 @@
+public class bucle_while {
+    public static void main(String[] args){
+        int numeroWhile=0;
+        while (numeroWhile<3) {
+            numeroWhile=numeroWhile+1;
+            System.out.println(" Repeticion nro "+numeroWhile);
+        } }
+}
